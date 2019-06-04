536ef073c3ffc677127be57c92340d083f41cab4
Mats <mats.klijn@live.com>
2019-06-04 12:29:04 +0200

    update noise

--- Noise.py
+++ Noise.py
@@ -18,4 +18,3 @@ SPL = 83.4 + 15.3*log(P_motor) - 20*log(diameter_blade) + 38.5*M_t - 3*(N_blades
 M_t = (pi * diameter_blade * RPM_rotor)/(60 * speed_of_sound)                                                                           #Propeller tip speed as a mach number
 f   = (N_blades * RPM_rotor)/60                                                                                                         #frequency of rotor system
 PNL = 40 + ((33 + 1/3) * log(noy))                                                                                                        #PNL value dependent on noy value. Noy values comes from the noise table found in the noise book
-
